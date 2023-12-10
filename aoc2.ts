4d68d32e1eb874aa31ea84d8a70510778668e127
Melisa <melisa@Slavas-MacBook-Air.local>
2023-12-10 08:50:26 -1000

    completed day 2 part 2

--- aoc2.ts
+++ aoc2.ts
@@ -44,3 +44,53 @@ function dayTwoPartOne() {
 }
 
 console.log(dayTwoPartOne());
+// For day 2 part 2, we need to figure out the minimum needed for each game and then multiply the values together 
+// and finally add all of the powers to get the final answer
+// attempt 1: 59795 correct! 
+function dayTwoPartTwo() {
+    // 
+    let sum = 0;
+    // Each line of dataset
+    data.forEach((game) => {
+        const minRequiredMap = {};
+        // separate game details from outcomes 
+        const lineSplit: string[] = game.split(":");
+        // all the rounds
+        const rounds: string[] = lineSplit[1].split(";");
+        let games: string[][] = rounds.map(round => round.split(","));
+        games = games.map((result) => result.map((str) => str.trim())
+        )
+        // split value of each turn into # and color 
+       games.forEach((round: string[]) => {
+            return round.forEach((valueCheck) => {
+                // check against mapping if color limit is <= # drawn
+                const [num, color] = valueCheck.split(" ");
+                const parsedNum = parseInt(num);
+                if (minRequiredMap[color]) {
+                    // check if new value smaller
+                    if (minRequiredMap[color] < parsedNum) {
+                        minRequiredMap[color] = parsedNum;
+                    }
+                } else {
+                    minRequiredMap[color] = parsedNum;
+                }
+            })
+        });
+        // loop through and get the power from mins 
+        console.log(minRequiredMap);
+        let pow: number = 0;
+        for (let color in minRequiredMap) {
+            const parsedMin = parseInt(minRequiredMap[color]);
+            if (pow) {
+                pow *= parsedMin;
+            } else {
+                pow = parsedMin;
+            }
+        }
+        console.log(pow);
+        sum += pow;
+    })
+    return sum;
+}
+
+console.log(dayTwoPartTwo());
